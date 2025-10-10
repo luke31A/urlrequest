f62cf294d0ecf35b9384e5a0ee3f9ed45bd0c2dd
luke31A <lukeadams248@gmail.com>
2025-10-10 12:42:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def find_sandbox_url(data_center: str, tenant_id: str):
         "Data Center 105": "https://wd105-impl.workday.com/wday/authgwy/{id}/login.htmld?redirect=n",
         "Data Center 501": "https://wd501-impl.workday.com/wday/authgwy/{id}/login.htmld?redirect=n",
         "Data Center 503": "https://impl.wd503.myworkday.com/wday/authgwy/{id}/login.htmld?redirect=n",
-        "Data Center 108": "https://impl.wd503.myworkday.com/wday/authgwy/{id}/login.htmld?redirect=n"
+        "Data Center 108": "https://impl.wd108.myworkday.com/wday/authgwy/{id}/login.htmld?redirect=n"
         
     }
     return sandbox_urls.get(data_center, None)
